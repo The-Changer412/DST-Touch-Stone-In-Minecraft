1bfc787ab58129c42fb2dbf519b0b53de40f9dbc
The-Changer412 <thechanger412@gmail.com>
2022-03-19 22:06:35 -0500

    change minecraft version to 1.18.2

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 ./gradlew build
 cd build/libs
-rm ~/.local/share/multimc/instances/'Fabric 1.18.1'/.minecraft/mods/touchstonedst-1.0.0.jar
-rm ~/.local/share/multimc/instances/'Fabric 1.18.1'/.minecraft/mods/touchstonedst-1.0.0-sources.jar
-cp touchstonedst-1.0.0.jar ~/.local/share/multimc/instances/'Fabric 1.18.1'/.minecraft/mods
-cp touchstonedst-1.0.0-sources.jar ~/.local/share/multimc/instances/'Fabric 1.18.1'/.minecraft/mods
+rm ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods/touchstonedst-1.0.0.jar
+rm ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods/touchstonedst-1.0.0-sources.jar
+cp touchstonedst-1.0.0.jar ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods
+cp touchstonedst-1.0.0-sources.jar ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods
 cd ~/.local/share/multimc
-./MultiMC -l "Fabric 1.18.1"
+./MultiMC -l "Fabric 1.18.2"
