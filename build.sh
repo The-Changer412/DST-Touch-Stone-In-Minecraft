03dc67bf0b89df0fc607bfc1f75dbe2014cd43be
The-Changer412 <thechanger412@gmail.com>
2022-03-26 18:20:54 -0500

    updated the project to 1.18.2

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 ./gradlew build
 cd build/libs
-rm ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods/touchstonedst-1.0.0.jar
-rm ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods/touchstonedst-1.0.0-sources.jar
-cp touchstonedst-1.0.0.jar ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods
-cp touchstonedst-1.0.0-sources.jar ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods
+rm ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods/Touch_Stone_DST-MOD-1.0.0_MC-1.18.2.jar
+cp Touch_Stone_DST-MOD-1.0.0_MC-1.18.2.jar ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods
 cd ~/.local/share/multimc
 ./MultiMC -l "Fabric 1.18.2"
