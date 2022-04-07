ba5c1b156794bf780aa9cf4766c878a3d5c424ed
The Changer <thechanger412@gmail.com>
2022-04-06 21:03:14 -0500

    removed unused files

--- build.sh
+++
@@ -1,6 +0,0 @@
-./gradlew build
-cd build/libs
-rm ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods/Touch_Stone_DST-MOD-1.0.0_MC-1.18.2.jar
-cp Touch_Stone_DST-MOD-1.0.0_MC-1.18.2.jar ~/.local/share/multimc/instances/'Fabric 1.18.2'/.minecraft/mods
-cd ~/.local/share/multimc
-./MultiMC -l "Fabric 1.18.2"
